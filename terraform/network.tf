015c742cdec412c07b5a631e1382f8b60553f8ca
Robin Mordasiewicz <robin@mordasiewicz.com>
2026-04-29 05:34:22 +0000

    feat: extract terraform files from inline docs into DRY terraform/ directory
    The deployment guide embedded all Terraform HCL and a 1,753-line cloud-init
    YAML inline in markdown code blocks. Extract all inline code into a proper
    terraform/ directory and rewire the MDX to use remark-code-import file=
    references. Checkov inline skip annotations are added to terraform resources
    for lab-environment Azure checks.
    
    Closes #70

---
+++ terraform/network.tf
@@ -0,0 +1,116 @@
+resource "azurerm_resource_group" "origin" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    environment = var.environment_tag
+    component   = "origin-server"
+  }
+}
+
+resource "azurerm_virtual_network" "origin" {
+  name                = "vnet-origin-server"
+  address_space       = ["10.200.0.0/16"]
+  location            = azurerm_resource_group.origin.location
+  resource_group_name = azurerm_resource_group.origin.name
+
+  tags = azurerm_resource_group.origin.tags
+}
+
+resource "azurerm_subnet" "origin" {
+  #checkov:skip=CKV2_AZURE_31:Lab subnet - NSG associated at NIC level
+  name                 = "snet-origin"
+  resource_group_name  = azurerm_resource_group.origin.name
+  virtual_network_name = azurerm_virtual_network.origin.name
+  address_prefixes     = ["10.200.1.0/24"]
+}
+
+resource "azurerm_public_ip" "origin" {
+  name                = "pip-origin-server"
+  location            = azurerm_resource_group.origin.location
+  resource_group_name = azurerm_resource_group.origin.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+  tags = azurerm_resource_group.origin.tags
+}
+
+resource "azurerm_network_security_group" "origin" {
+  #checkov:skip=CKV_AZURE_10:Lab NSG - SSH open for demo access
+  #checkov:skip=CKV_AZURE_160:Lab NSG - HTTP port 80 required for traffic
+  #checkov:skip=CKV_AZURE_220:Lab NSG - SSH open for demo access
+  name                = "nsg-origin-server"
+  location            = azurerm_resource_group.origin.location
+  resource_group_name = azurerm_resource_group.origin.name
+
+  security_rule {
+    name                       = "AllowHTTP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowHTTPS"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowCrAPI"
+    priority                   = 130
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8888"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = azurerm_resource_group.origin.tags
+}
+
+resource "azurerm_network_interface" "origin" {
+  #checkov:skip=CKV_AZURE_119:Lab NIC - public IP required for demo access
+  name                = "nic-origin-server"
+  location            = azurerm_resource_group.origin.location
+  resource_group_name = azurerm_resource_group.origin.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.origin.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.origin.id
+  }
+
+  tags = azurerm_resource_group.origin.tags
+}
+
+resource "azurerm_network_interface_security_group_association" "origin" {
+  network_interface_id      = azurerm_network_interface.origin.id
+  network_security_group_id = azurerm_network_security_group.origin.id
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,64 @@
+output "public_ip" {
+  description = "Public IP address of the origin server"
+  value       = azurerm_public_ip.origin.ip_address
+}
+
+output "ssh_command" {
+  description = "SSH command to connect to the origin server"
+  value       = "ssh ${var.admin_username}@${azurerm_public_ip.origin.ip_address}"
+}
+
+output "origin_url" {
+  description = "Base HTTP URL of the origin server"
+  value       = "http://${azurerm_public_ip.origin.ip_address}"
+}
+
+output "health_check_url" {
+  description = "Health check endpoint"
+  value       = "http://${azurerm_public_ip.origin.ip_address}/health"
+}
+
+output "juice_shop_url" {
+  description = "OWASP Juice Shop URL"
+  value       = "http://${azurerm_public_ip.origin.ip_address}/juice-shop/"
+}
+
+output "dvwa_url" {
+  description = "DVWA URL"
+  value       = "http://${azurerm_public_ip.origin.ip_address}/dvwa/"
+}
+
+output "vampi_url" {
+  description = "VAmPI URL"
+  value       = "http://${azurerm_public_ip.origin.ip_address}/vampi/"
+}
+
+output "httpbin_url" {
+  description = "httpbin URL"
+  value       = "http://${azurerm_public_ip.origin.ip_address}/httpbin/"
+}
+
+output "whoami_url" {
+  description = "whoami request diagnostics URL"
+  value       = "http://${azurerm_public_ip.origin.ip_address}/whoami/"
+}
+
+output "dvga_url" {
+  description = "DVGA GraphQL security URL"
+  value       = "http://${azurerm_public_ip.origin.ip_address}/dvga/"
+}
+
+output "restaurant_url" {
+  description = "RESTaurant API security URL"
+  value       = "http://${azurerm_public_ip.origin.ip_address}/restaurant/"
+}
+
+output "crapi_url" {
+  description = "crAPI microservices security URL"
+  value       = "http://${azurerm_public_ip.origin.ip_address}:8888"
+}
+
+output "resource_group" {
+  description = "Resource group containing all origin server resources"
+  value       = azurerm_resource_group.origin.name
+}
